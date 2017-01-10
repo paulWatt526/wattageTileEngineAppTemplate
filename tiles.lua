9387166d53c5e62d574ff4aa38cbf45841485a18
paulWatt526 <necro526>
2017-01-10 13:48:13 +0800

    Updated tile images.

--- tiles.lua
+++ tiles.lua
@@ -1,3 +1,13 @@
+--
+-- created with TexturePacker (http://www.codeandweb.com/texturepacker)
+--
+-- $TexturePacker:SmartUpdate:82e7fdcf46f55335e0f1d3d1c8f7a2cf:584532efb031556035292817a3f01ab9:f4492607ea55a754477543692c89a688$
+--
+-- local sheetInfo = require("mysheet")
+-- local myImageSheet = graphics.newImageSheet( "mysheet.png", sheetInfo:getSheet() )
+-- local sprite = display.newSprite( myImageSheet , {frames={sheetInfo:getFrameIndex("sprite")}} )
+--
+
 local SheetInfo = {}
 
 SheetInfo.sheet =
@@ -28,9 +38,17 @@ SheetInfo.sheet =
             height=32,
 
         },
+        {
+            -- tiles_3
+            x=103,
+            y=1,
+            width=32,
+            height=32,
+
+        },
     },
     
-    sheetContentWidth = 102,
+    sheetContentWidth = 136,
     sheetContentHeight = 34
 }
 
@@ -40,6 +58,7 @@ SheetInfo.frameIndex =
     ["tiles_0"] = 1,
     ["tiles_1"] = 2,
     ["tiles_2"] = 3,
+    ["tiles_3"] = 4,
 }
 
 function SheetInfo:getSheet()
